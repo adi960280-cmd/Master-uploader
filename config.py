1f8b68db1d9706b0f669605c42d74c6618981cc0
adi960280-cmd <adityamalhotra960@gmail.com>
2025-10-23 19:43:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # Ensure correct key name
-    API_ID = int(os.environ.get("API_ID", ""))  # Added key name and default value
-    API_HASH = os.environ.get("API_HASH", "")  # Added key name for consistency
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8316278584:AAE_hOihEQ0DjFCr1tbG4Eet-6c3qkyLotM")  # Ensure correct key name
+    API_ID = int(os.environ.get("API_ID", "29115102"))  # Added key name and default value
+    API_HASH = os.environ.get("API_HASH", "1a331db2b00e9d2decaa9c7276449eb6")  # Added key name for consistency
 
-    AUTH_USER = os.environ.get("AUTH_USERS", "1").split(',')
+    AUTH_USER = os.environ.get("AUTH_USERS", "1224092270").split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]  # Ensuring list of integers
 
     HOST = os.environ.get("HOST", "https://api.masterapi.tech")  # Keeping HOST configurable
     CREDIT = os.environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")  # Making CREDIT an environment variable for flexibility
+
 
 
 
